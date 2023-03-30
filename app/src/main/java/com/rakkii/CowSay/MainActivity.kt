f3c4a0198b21098a8f20d50a5d0cdb5dc3403b35
luisaraujoc <luislk_@outlook.com>
2023-03-30 11:33:36 -0300

    pre-final app

--- app/src/main/java/com/rakkii/CowSay/MainActivity.kt
+++ app/src/main/java/com/rakkii/CowSay/MainActivity.kt
@@ -12,9 +12,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.platform.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -45,6 +45,7 @@ fun MyApp(content: @Composable () -> Unit) {
         content()
     }
 }
+
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterialApi::class)
 @Composable
